f06a9855634db265e06e3841c308302f50d62920
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2021-07-07 08:07:23 +0300

    replace credentials for deploy

--- deploy/index.mjs
+++ deploy/index.mjs
@@ -1,4 +1,8 @@
+import dotenv from "dotenv"
 import { remote } from "webdriverio";
+dotenv.config();
+
+const { STUDIO_USERNAME, STUDIO_PASSWORD, STUDIO_LINK } = process.env;
 
 let browser;
 (async () => {
@@ -6,7 +10,7 @@ let browser;
     capabilities: { browserName: "chrome" },
   });
 
-  await browser.navigateTo("");
+  await browser.navigateTo(`${STUDIO_LINK}#/login`);
 
   const userNameInput = await browser.$("[ng-model=username]");
   await userNameInput.setValue(STUDIO_USERNAME);
@@ -17,14 +21,14 @@ let browser;
   const submitButton = await browser.$("button.btn-primary");
   await submitButton.click();
 
-//   const link = await browser.$('[ng-click="selectCampaign(campaign)"]');
-//   await link.click();
+  // const link = await browser.$('.campaign .name');
+  // await link.click();
 
-  await browser.navigateTo(
-    "http://studio3.inskinmedia.com/#/build/60debff2606883720a514cb5/code"
-  );
+  // await browser.navigateTo(
+  //   `${STUDIO_LINK}#/build/60debff2606883720a514cb5/code`
+  // );
 
-  console.log(await browser.getTitle()); // outputs "WebdriverIO - Google Search"
+  console.log(await browser.getTitle());
 
   // await browser.deleteSession()
 })().catch((err) => {
