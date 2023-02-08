0276623c10bfbbf46152c53dd47a8542786c225e
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2023-02-08 19:27:22 +0100

    replace requestAnimFrame to setTimeout

--- modules/slider/src/utils/fps-loop.js
+++ modules/slider/src/utils/fps-loop.js
@@ -1,14 +1,9 @@
 export function fpsLoop(cb, fps = 60) {
-  var prevTime = 0;
   var frameRate = 1000 / fps;
 
-  function animate(time) {
-    if (time > prevTime + frameRate) {
-      prevTime = time;
-      cb();
-    }
-
-    requestAnimationFrame(animate);
+  function animate() {
+    cb();
+    setTimeout(animate, frameRate);
   }
 
   animate();
